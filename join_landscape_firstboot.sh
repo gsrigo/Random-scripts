b1e930da39e981c11fd6f179b7fe634074db1cb6
picolin <sanz.rigo@gmail.com>
2017-07-21 10:41:17 -0400

    Create join_landscape_firstboot.sh
    To be run on Ubuntu machines, helpful when there is a template VM

---
+++ join_landscape_firstboot.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+#This script joins new VMs to the landscape server and it resides on the 
+
+landscape_config(){
+
+/usr/bin/landscape-config --silent \
+--computer-title "$(hostname)" \
+--ssl-public-key=/etc/ssl/certs/landscape_key.pem \
+--account-name standalone -p jhuapl-landscape \
+--script-users=ALL \
+--url https://apl-landscape.jhuapl.edu/message-system \
+--ping-url http://apl-landscape.jhuapl.edu/ping \
+--tags=xenial16
+}
+
+ISINSTALLED="/usr/bin/landscape-client"
+ISHOSTBASE=$(hostname)
+CONFFILE="/etc/landscape/client.conf"
+
+if [[ ${ISHOSTBASE,,} = *base* ]]; then
+        exit 0
+
+elif [ -e $CONFFILE ] && [ -e $ISINSTALLED ]; then
+        cat /dev/null > $CONFFILE
+        landscape_config
+else
+        apt update
+        apt -y install landscape-client
+        if [ $? -eq 0 ]; then
+                landscape_config
+                rm -f /etc/init.d/aplfirstboot
+        else
+                echo "Landscape client package could not be installed"
+                exit 1
+        fi
+fi

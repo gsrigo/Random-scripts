5e3892da608a03d6c383408c61487da2c572093b
picolin <sanz.rigo@gmail.com>
2017-10-05 10:20:17 -0400

    Update add__user_sshkeys_passwordlesssudo_.sh
    Note that this script is meant for high privilege users.

--- add__user_sshkeys_passwordlesssudo_.sh
+++ add__user_sshkeys_passwordlesssudo_.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 
-#This script will add the necessary info/files in order for ansible to have access to the client host.
+#This script will add the necessary info/files in order for $USER to have access to the client host.
 
 USER="ansible"
 DIR="/home/$USER/.ssh/"
 ID_RSA_PUB="ssh-rsa blahblah"
+
+#Creating a user
 create_user()
 {
-
 if [ ! "$(id -u $USER 2> /dev/null)" ]; then
 	if [ -f /etc/lsb-release ]; then
 		useradd -m -d /home/$USER -s /bin/bash $USER &&
@@ -17,12 +18,12 @@ if [ ! "$(id -u $USER 2> /dev/null)" ]; then
                 useradd -m -d /home/$USER -s /bin/bash $USER &&
                 echo "User created"
 	fi
-
 else
 	echo "User exists. Checking ssh folder..."
 fi
 }
 
+#Creating ssh dir and copying the ssh key
 create_ssh_dir()
 {
 if [ ! -f "$DIR"/authorized_keys ]; then
@@ -37,8 +38,15 @@ else
 fi
 }
 
+
+#Adding an entry to the sudoers file for passwordless sudo of this user
 passwordless_access()
 {
 	#sed -i '/includedir/s/^#//g' /etc/sudoers
 	echo "$USER	ALL = NOPASSWD: ALL" > /etc/sudoers.d/$USER	
 }
+
+#Running...
+create_user
+create_ssh_dir
+passwordless_access

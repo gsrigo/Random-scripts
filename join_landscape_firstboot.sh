a8ff68dac7d767eda07fae31076f6af4a5d35aa4
picolin <sanz.rigo@gmail.com>
2017-10-11 11:27:45 -0400

    Update join_landscape_firstboot.sh
    - note that this script is useful for VM templates. not so much for existing machines.
    - Templates are usually named *base*
    - this can create duplicates on systems that have already been registered and runs the script again.

--- join_landscape_firstboot.sh
+++ join_landscape_firstboot.sh
@@ -1,24 +1,34 @@
 #!/usr/bin/env bash
 
-#This script joins new VMs to the landscape server and it resides on the 
-
-landscape_config(){
-
-#Chanage tag accordingly!!!
-
-/usr/bin/landscape-config --silent \
---computer-title "$(hostname)" \
---ssl-public-key=/etc/ssl/certs/landscape_key.pem \
---account-name standalone -p jhuapl-landscape \
---script-users=ALL \
---url https://apl-landscape.jhuapl.edu/message-system \
---ping-url http://apl-landscape.jhuapl.edu/ping \
---tags=generic
-}
+#This script joins new VMs to the landscape server.
+#Script is activated on templates with #update-rc.d aplfirstboot defaults
 
 ISINSTALLED="/usr/bin/landscape-client"
 ISHOSTBASE=$(hostname)
 CONFFILE="/etc/landscape/client.conf"
+
+landscape_config(){
+
+        /usr/bin/landscape-config --silent \
+        --computer-title "$(hostname)" \
+        --ssl-public-key=/etc/ssl/certs/landscape_key.pem \
+        --account-name standalone -p jhuapl-landscape \
+        --script-users=ALL \
+        --url https://apl-landscape.jhuapl.edu/message-system \
+        --ping-url http://apl-landscape.jhuapl.edu/ping \
+        --tags=xenial16,dev
+}
+
+remove_script(){
+        if [ $? -eq 0 ]; then
+                update-rc.d -f aplfirstboot remove
+                rm -f /etc/init.d/aplfirstboot
+        else
+                echo "Landscape client package could not be installed" > /tmp/landscape_error
+                exit 1
+        fi
+}
+
 
 if [[ ${ISHOSTBASE,,} = *base* ]]; then
         exit 1
@@ -26,14 +36,11 @@ if [[ ${ISHOSTBASE,,} = *base* ]]; then
 elif [ -e $CONFFILE ] && [ -e $ISINSTALLED ]; then
         cat /dev/null > $CONFFILE
         landscape_config
+        remove_script
+
 else
         apt update
         apt -y install landscape-client
-        if [ $? -eq 0 ]; then
-                landscape_config
-                rm -f /etc/init.d/aplfirstboot
-        else
-                echo "Landscape client package could not be installed"
-                exit 1
-        fi
+	landscape_config
+        remove_script
 fi
